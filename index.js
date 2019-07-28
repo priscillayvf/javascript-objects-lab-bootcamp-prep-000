8e8570fb71788ca9ef8ead5723a008e40d253229
Priscilla Firpo <priscillayf@gmail.com>
2019-07-28 21:53:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object)
   delete newObject.key
-  return Object.assign({}, newObject, key)
+  return newObject
 }
