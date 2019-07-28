7a58c659cacd445f69315d8926142e539ea8fcb9
Priscilla Firpo <priscillayf@gmail.com>
2019-07-28 21:32:35 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var recipes = {
+  "prop": "1",
+  "prop2": "2"
+}
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+
+}
