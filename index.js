144c9f8ad1f73e1fd2d95f52f01781fba4ad2d32
Priscilla Firpo <priscillayf@gmail.com>
2019-07-28 22:50:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function deleteFromObjectByKey(object, key) {
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
   var newObject = delete object.key
-  return object
+  return newObject
 }
